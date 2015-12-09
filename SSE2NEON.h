2b608ccae0e739ebabd40c77a087fdf318f5b856
jratcliff63367 <jratcliffscarab@gmail.com>
2015-12-09 10:45:52 -0600

    Adding the MIT license to the header file

--- SSE2NEON.h
+++ SSE2NEON.h
@@ -17,7 +17,11 @@
 //
 //	https://github.com/jratcliff63367/sse2neon
 //
-// TipJar: 1PzgWDSyq4pmdAXRH8SPUtta4SWGrt4B1p : https://blockchain.info/address/1PzgWDSyq4pmdAXRH8SPUtta4SWGrt4B1p
+// Show your appreciation for open source by sending me a bitcoin tip to the following
+// address.
+//
+// TipJar: 1PzgWDSyq4pmdAXRH8SPUtta4SWGrt4B1p : 
+// https://blockchain.info/address/1PzgWDSyq4pmdAXRH8SPUtta4SWGrt4B1p
 //
 // 
 // Contributors to this project are:
@@ -25,6 +29,28 @@
 // John W. Ratcliff : jratcliffscarab@gmail.com
 // Brandon Rowlett : browlett@nvidia.com
 // Ken Fast : kfast@gdeb.com
+//
+//
+/*
+** The MIT license:
+**
+** Permission is hereby granted, MEMALLOC_FREE of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, and to permit persons to whom the Software is furnished
+** to do so, subject to the following conditions:
+**
+** The above copyright notice and this permission notice shall be included in all
+** copies or substantial portions of the Software.
+
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+** WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+** CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #define GCC 1
 #define ENABLE_CPP_VERSION 0
