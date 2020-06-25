757048e87ff7c43a5c056264c0f11aea3cdb08c7
Jim Huang <jserv@biilabs.io>
2020-06-25 11:17:21 +0800

    Use the mirror for GNU Toolchain rather than Arm Developer site
    We suffered from various regressions because of unexpected connections
    to Arm Developer site while downloading prebuilt GNU Toolchain for Arm
    A-profile cores. Now, we migrate to our own mirror on GitHub stored with
    Git LFS, which makes CI/CD much faster and stable.

--- .ci/cross-tool.sh
+++ .ci/cross-tool.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ARM_DEV=https://developer.arm.com/-/media/Files/downloads/gnu-a
+ARM_MIRROR=https://github.com/DLTcollab/toolchain-arm/raw/master
 GCC_REL=9.2-2019.12
 
 MACHINE_TYPE=`uname -m`
@@ -18,9 +18,9 @@ set -x
 sudo apt-get install -y curl xz-utils qemu-user
 
 curl -L \
-    ${ARM_DEV}/${GCC_REL}/binrel/gcc-arm-${GCC_REL}-x86_64-arm-none-linux-gnueabihf.tar.xz \
+    ${ARM_MIRROR}/gcc-arm-${GCC_REL}-x86_64-arm-none-linux-gnueabihf.tar.xz \
     | tar -Jx || exit 1
 
 curl -L \
-    ${ARM_DEV}/${GCC_REL}/binrel/gcc-arm-${GCC_REL}-x86_64-aarch64-none-linux-gnu.tar.xz \
+    ${ARM_MIRROR}/gcc-arm-${GCC_REL}-x86_64-aarch64-none-linux-gnu.tar.xz \
     | tar -Jx || exit 1
