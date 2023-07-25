2465ec8a7fb1d5c2273a0ceb49799bf22f421471
Cuda-Chen <clh960524@gmail.com>
2023-07-25 16:08:09 +0800

    Fix _mm_cmplt_sd and _mm_cmpnlt_sd test cases

--- tests/impl.cpp
+++ tests/impl.cpp
@@ -4052,7 +4052,7 @@ result_t test_mm_cmplt_sd(const SSE2NEONTestImpl &impl, uint32_t iter)
 {
     double *_a = (double *) impl.mTestFloatPointer1;
     double *_b = (double *) impl.mTestFloatPointer2;
-    uint64_t d0 = (_a[0] <= _b[0]) ? ~UINT64_C(0) : 0;
+    uint64_t d0 = (_a[0] < _b[0]) ? ~UINT64_C(0) : 0;
     uint64_t d1 = ((uint64_t *) _a)[1];
 
     __m128d a = load_m128d(_a);
@@ -4194,7 +4194,7 @@ result_t test_mm_cmpnlt_sd(const SSE2NEONTestImpl &impl, uint32_t iter)
 {
     double *_a = (double *) impl.mTestFloatPointer1;
     double *_b = (double *) impl.mTestFloatPointer2;
-    uint64_t d0 = !(_a[0] <= _b[0]) ? ~UINT64_C(0) : 0;
+    uint64_t d0 = !(_a[0] < _b[0]) ? ~UINT64_C(0) : 0;
     uint64_t d1 = ((uint64_t *) _a)[1];
 
     __m128d a = load_m128d(_a);
