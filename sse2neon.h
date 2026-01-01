92f6de174717aef09033ad21568d5bb9e5470404
Jim Huang <jserv@ccns.ncku.edu.tw>
2026-01-01 19:44:13 +0800

    Bump copyright year

--- sse2neon.h
+++ sse2neon.h
@@ -4,7 +4,7 @@
 /*
  * sse2neon is freely redistributable under the MIT License.
  *
- * Copyright (c) 2015-2025 SSE2NEON Contributors.
+ * Copyright (c) 2015-2026 SSE2NEON Contributors.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
